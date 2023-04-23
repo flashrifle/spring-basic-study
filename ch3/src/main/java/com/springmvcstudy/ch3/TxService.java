88b33db43011f09ef44ba8ea69babd5014d92d03
leejaemin <flashrifle@gmail.com>
2023-04-24 00:03:05 +0900

    [스프링의 정석] TxService 수정

--- ch3/src/main/java/com/springmvcstudy/ch3/TxService.java
+++ ch3/src/main/java/com/springmvcstudy/ch3/TxService.java
@@ -4,55 +4,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionDefinition;
-import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
-
-import javax.sql.DataSource;
-
 @Service
-public class TxService {
-   @Autowired DataSource ds;
-
-    @Autowired A1Dao a1Dao;
+class TxServiceB {
     @Autowired B1Dao b1Dao;
-
-    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
-    public void insertA1WithTx() throws Exception {
-        PlatformTransactionManager tm = new DataSourceTransactionManager(ds);
-        DefaultTransactionDefinition txd = new DefaultTransactionDefinition();
-        txd.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
-        TransactionStatus status = tm.getTransaction(txd);
-        try {
-            a1Dao.insert(1,100);    // ok
-            insertB1WithTx();
-            a1Dao.insert(2,200);    // ok
-            tm.commit(status);
-        } catch (Exception e) {
-            e.printStackTrace();
-            tm.rollback(status);
-        } finally {
-        }
-    }
 
     @Transactional(propagation = Propagation.REQUIRES_NEW, rollbackFor = Exception.class)
     public void insertB1WithTx() throws Exception {
-        PlatformTransactionManager tm = new DataSourceTransactionManager(ds);
-        DefaultTransactionDefinition txd = new DefaultTransactionDefinition();
-        txd.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
-        TransactionStatus status = tm.getTransaction(txd);
-        try {
-            b1Dao.insert(1,100);    // ok
-            b1Dao.insert(1,200);    // fail
-            tm.commit(status);
-        } catch (Exception e) {
-            e.printStackTrace();
-            tm.rollback(status);
-        } finally {
-        }
+        b1Dao.insert(1,100);    // ok
+        b1Dao.insert(1,200);    // false
     }
+
+}
+@Service
+public class TxService {
+    @Autowired A1Dao a1Dao;
+    @Autowired B1Dao b1Dao;
+
+    @Autowired TxServiceB txServiceB;
+
+    @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
+    public void insertA1WithTx() throws Exception {
+        a1Dao.insert(1,100);    // ok
+        txServiceB.insertB1WithTx();
+        a1Dao.insert(2,200);    // ok
+    }
+
 
     public void insertA1WithoutTx() throws Exception {
         a1Dao.insert(1, 100);
